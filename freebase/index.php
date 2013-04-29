922cb05405cc6898ac22b12dbd9e2bd2974a5a68
Roy Russo <royrusso@gmail.com>
2013-04-29 15:32:30 -0400

    -- initial readme

--- freebase/index.php
+++ freebase/index.php
@@ -1,4 +1,5 @@
-<!--
+<?php
+/**************************************************************************
 Copyright 2013 Roy Russo
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,4 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 Latest Builds: https://github.com/royrusso
--->
+ **************************************************************************/
+
+
+?>
